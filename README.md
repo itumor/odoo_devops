5fa2b44ab1196be577768a3f26815bf9bb2855b4
root <root@ip-172-31-72-119.ec2.internal>
2017-05-31 09:32:01 +0000

    install odoo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+
 # odoo_devops
 odoo_devops
+1)chmod 777 install.sh
+2)./install.sh
+3)docker-compose up -d
+
---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+sudo apt install htop -y
